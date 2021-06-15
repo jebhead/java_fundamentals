9480738a28b53dcd839e233e006270e8e1fdb9fe
Jeb Head <jebhead@Jebs-MacBook-Pro.local>
2021-06-15 14:55:38 -0400

    Finished Exercise_4

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,26 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
-
         // write your code below
+
+        int i = 1;
+        int j = 2;
+        short g = 3;
+        long l = 44553;
+        boolean tf = true;
+        char letr = 'a';
+        float r = 4.4f;
+
+        System.out.println("int i is: " + i);
+        System.out.println("int j is: " + j);
+        System.out.println("short g is: " + g);
+        System.out.println("long l is : " + l);
+        System.out.println("whether true or not is " + tf);
+        System.out.println("the letter of the day is: " + letr);
+        System.out.println("to be more precise ... : " + r);
+
+
+
 
     }
 
