e65adf66e81e6f3e50da769be378c9e68fa93f02
Jeb Head <jebhead@Jebs-MacBook-Pro.local>
2021-08-09 11:12:01 -0400

    Object lab Textile

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Textile.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Textile.java
@@ -17,10 +17,28 @@ public class Textile {
         Braid b3 = new Braid("fish_4", 4, 78, 675, 3.2, 0.8, sp2);
         Braid b4 = new Braid("rope_48", 48, 14, 54, 84.3, 12.8, sp1);
 
+        B_Order o1 = new B_Order(b3,"Joe's", 354, 33.43);
+        B_Order o2 = new B_Order(b2,"Yankee", 1234, 14.43);
+        B_Order o3 = new B_Order(b4,"Climb Palace", 1354, 9.65);
+        B_Order o4 = new B_Order(b1,"TRX", 745, 11.73);
+        B_Order o5 = new B_Order(b3,"Pure", 212, 27.55);
+
+        double rev1 = calcSale(o1.getbPricePerLb(), o1.getoLbs());
+        double rev2 = calcSale(o2.getbPricePerLb(), o2.getoLbs());
+        double rev3 = calcSale(o3.getbPricePerLb(), o3.getoLbs());
+        double rev4 = calcSale(o4.getbPricePerLb(), o4.getoLbs());
+        double rev5 = calcSale(o5.getbPricePerLb(), o5.getoLbs());
 
         System.out.println(sp1.yarn.getyName() + " has tensile of  " + sp1.yarn.getTensile());
+    }
+
+    public static double calcSale(double price, double lbs) {
 
 
+
+        double saleDollars = price * lbs;
+        System.out.println("sale is " + saleDollars);
+        return  saleDollars;
     }
 
 
@@ -252,14 +270,20 @@ class Braid {
     }
 }
 
-class Order_brd {
+class B_Customer {
+
+
+}
+
+
+class B_Order {
 
     Braid braid;
     private String bCustomer;
     private double oLbs;
     private double bPricePerLb;
 
-    public Order_brd(Braid braid, String bCustomer, double oLbs, double bPricePerLb) {
+    public B_Order(Braid braid, String bCustomer, double oLbs, double bPricePerLb) {
         this.braid = braid;
         this.bCustomer = bCustomer;
         this.oLbs = oLbs;
