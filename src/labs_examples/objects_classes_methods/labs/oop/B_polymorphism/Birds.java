3c92e13d9da1dd4d301b4093a9544b13c0fd4fd2
Jeb Head <jebhead@jebs-mbp.lan>
2021-09-05 10:30:46 -0400

    Polymorphisms

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Birds.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Birds.java
@@ -1,2 +1,59 @@
-package labs_examples.objects_classes_methods.labs.oop.B_polymorphism;public class Birds {
+package labs_examples.objects_classes_methods.labs.oop.B_polymorphism;
+
+public class Birds {
+
+    private double weight;
+    private double flightSpeed;
+    private double walkSpeed;
+
+    public Birds(double weight, double flightSpeed, double walkSpeed) {
+        this.weight = weight;
+        this.flightSpeed = flightSpeed;
+        this.walkSpeed = walkSpeed;
+    }
+}
+
+class Flying extends Birds {
+
+    public Flying(double weight, double flightSpeed, double walkSpeed) {
+        super(weight, flightSpeed, walkSpeed);
+
+        double range = maxRange(flightSpeed, 12);
+        System.out.println("Max flight range is: " + range);
+    }
+
+    public double maxRange(double fspd, int x) {
+
+        double fRange = fspd * x;
+        return fRange;
+    }
+}
+
+class Flightless extends Birds {
+
+    public Flightless(double weight, double flightSpeed, double walkSpeed) {
+        super(weight, flightSpeed, walkSpeed);
+
+        double range = maxRange(walkSpeed, 8);
+        System.out.println("Max walk range is: " + range);
+    }
+
+    public double maxRange(double fspd, int x) {
+
+        double wRange = fspd * x;
+        return wRange;
+    }
+}
+
+class Range_Control {
+
+    public static void main(String[] args) {
+
+        Birds gull = new Flying(22.1, 32.4, 3.2);
+        Birds robin = new Flying(17.1, 28.4, 3.2);
+
+        Birds emu = new Flightless(164.4, 0, 6.5);
+        Birds ostrich = new Flightless(264.4, 0, 9.5);
+
+    }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/NumberGame.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/NumberGame.java
@@ -1,2 +1,110 @@
-package labs_examples.objects_classes_methods.labs.oop.B_polymorphism;public class NumberGame {
+package labs_examples.objects_classes_methods.labs.oop.B_polymorphism;
+
+import java.util.Random;
+
+public class NumberGame {
+
+    public static void main(String[] args) {
+
+        Player p1 = new Player(4, 7, 5);
+        Player p2 = new Player(6, 8, 2);
+
+        int score_1_1 = p1.getSkill_1() * genRand();
+        int score_1_2 = p1.getSkill_2() * genRand();
+        int score_1_3 = p1.getSkill_3() * genRand();
+
+        int p1_gameScore = score_1_1 + score_1_2 + score_1_3;
+
+        int score_2_1 = p2.getSkill_1() * genRand();
+        int score_2_2 = p2.getSkill_2() * genRand();
+        int score_2_3 = p2.getSkill_3() * genRand();
+
+        int p2_gameScore = score_2_1 + score_2_2 + score_2_3;
+
+        System.out.println("player 1 scores: " + p1_gameScore);
+        System.out.println("player 2 scores: " + p2_gameScore);
+
+        if(p2_gameScore < p1_gameScore) {
+            System.out.println("Player 1 wins");
+        } else System.out.println("Player 2 wins");
+
+
+        int p1_otherScore = genRand(p1.getSkill_1(), p1.getSkill_2(), p1.getSkill_3());
+        int p2_otherScore = genRand(p2.getSkill_1(), p2.getSkill_2(), p2.getSkill_3());
+
+        System.out.println("Player 1 other score " + p1_otherScore);
+        System.out.println("Player 2 other score " + p2_otherScore);
+
+        if(p2_otherScore < p1_otherScore) {
+            System.out.println("Player 1 wins");
+        } else System.out.println("Player 2 wins");
+
+
+    }
+
+    public static int genRand() {
+
+        Random rand = new Random();
+
+        int upperbound = 10;
+
+        int ranInt = rand.nextInt(upperbound);
+
+        System.out.println("ranInt is " + ranInt);
+
+        return ranInt;
+
+    }
+
+    public static int genRand(int s1, int s2, int s3) {
+
+        Random rand = new Random();
+
+        int upperbound = 10;
+
+        int ranInt = rand.nextInt(upperbound);
+
+        int score = ranInt * (s1 + s2 + s3);
+
+
+        return score;
+
+    }
+}
+
+class Player  {
+
+    private int skill_1;
+    private int skill_2;
+    private int skill_3;
+
+    public Player(int skill_1, int skill_2, int skill_3) {
+        this.skill_1 = skill_1;
+        this.skill_2 = skill_2;
+        this.skill_3 = skill_3;
+    }
+
+    public int getSkill_1() {
+        return skill_1;
+    }
+
+    public void setSkill_1(int skill_1) {
+        this.skill_1 = skill_1;
+    }
+
+    public int getSkill_2() {
+        return skill_2;
+    }
+
+    public void setSkill_2(int skill_2) {
+        this.skill_2 = skill_2;
+    }
+
+    public int getSkill_3() {
+        return skill_3;
+    }
+
+    public void setSkill_3(int skill_3) {
+        this.skill_3 = skill_3;
+    }
 }
